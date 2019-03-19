fb3e7a7da33cec9b3266b7136dba90c9f8683d22
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-19 16:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
   var itemPrice = Math.floor((Math.random * 100) + 1)
  var item = `{ [itemName]: name of the item, [itemPrice]: price of the item, }`
- return items
+ return item
 }
 
 function viewCart() {
